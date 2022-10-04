eaeae58c29e299d4acca52c02043aee76094b25d
Rkpani05 <107294186+Rkpani05@users.noreply.github.com>
2022-10-05 00:55:12 +0530

    modified the code

--- C++/Sorting/insertionSort.cpp
+++ C++/Sorting/insertionSort.cpp
@@ -1,28 +1,50 @@
-#include <iostream>
+// C++ program for insertion sort
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-	int n;
-	cin >> n;
+// Function to sort an array using
+// insertion sort
+void insertionSort(int arr[], int n)
+{
+	int i, key, j;
+	for (i = 1; i < n; i++)
+	{
+		key = arr[i];
+		j = i - 1;
 
-	int a[n];
-	for(int i=1; i<n; i++){
-		cin >> a[i];
-	}
-
-	for(int i=0; i<n; i++){
-		int current = a[i];
-		int prev = i - 1;
-
-		while(prev >= 0 and a[prev] > current){
-			a[prev + 1] = a[prev];
-			prev -= 1; 
+		// Move elements of arr[0..i-1],
+		// that are greater than key, to one
+		// position ahead of their
+		// current position
+		while (j >= 0 && arr[j] > key)
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
 		}
-		a[prev + 1] = current;
+		arr[j + 1] = key;
 	}
+}
 
-	for(int i=0; i<n; i++){
-		cout << a[i] << " ";
-	}
+// A utility function to print an array
+// of size n
+void printArray(int arr[], int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+// Driver code
+int main()
+{
+	int arr[] = { 12, 11, 13, 5, 6 };
+	int N = sizeof(arr) / sizeof(arr[0]);
+
+	insertionSort(arr, N);
+	printArray(arr, N);
+
 	return 0;
 }
+// This is code is contributed by rathbhupendra
