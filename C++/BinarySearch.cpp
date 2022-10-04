f7ca6a89f1ea8fb25a4af633afd48eb8aa4bb871
Rkpani05 <107294186+Rkpani05@users.noreply.github.com>
2022-10-05 00:51:43 +0530

    Updated

--- C++/BinarySearch.cpp
+++ C++/BinarySearch.cpp
@@ -1,34 +1,43 @@
-// Binary Search in C++
-
-#include <iostream>
+// C++ program to implement recursive Binary Search
+#include <bits/stdc++.h>
 using namespace std;
 
-int binarySearch(int array[], int x, int low, int high) {
-  
-	// Repeat until the pointers low and high meet each other
-  while (low <= high) {
-    int mid = low + (high - low) / 2;
+// A recursive binary search function. It returns
+// location of x in given array arr[l..r] is present,
+// otherwise -1
+int binarySearch(int arr[], int l, int r, int x)
+{
+	if (r >= l) {
+		int mid = l + (r - l) / 2;
 
-    if (array[mid] == x)
-      return mid;
+		// If the element is present at the middle
+		// itself
+		if (arr[mid] == x)
+			return mid;
 
-    if (array[mid] < x)
-      low = mid + 1;
+		// If element is smaller than mid, then
+		// it can only be present in left subarray
+		if (arr[mid] > x)
+			return binarySearch(arr, l, mid - 1, x);
 
-    else
-      high = mid - 1;
-  }
+		// Else the element can only be present
+		// in right subarray
+		return binarySearch(arr, mid + 1, r, x);
+	}
 
-  return -1;
+	// We reach here when element is not
+	// present in array
+	return -1;
 }
 
-int main(void) {
-  int array[] = {3, 1, 2, 4, 2, 10, 4};
-  int x = 4;
-  int n = sizeof(array) / sizeof(array[0]);
-  int result = binarySearch(array, x, 0, n - 1);
-  if (result == -1)
-    printf("Not found");
-  else
-    printf("Element is found at index %d", result);
+int main(void)
+{
+	int arr[] = { 2, 3, 4, 10, 40 };
+	int x = 10;
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int result = binarySearch(arr, 0, n - 1, x);
+	(result == -1)
+		? cout << "Element is not present in array"
+		: cout << "Element is present at index " << result;
+	return 0;
 }
