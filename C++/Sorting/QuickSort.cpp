1f8c945a23c2322800f1758476d33a5730556906
Rkpani05 <107294186+Rkpani05@users.noreply.github.com>
2022-10-05 00:57:08 +0530

    Quicksort

--- C++/Sorting/QuickSort.cpp
+++ C++/Sorting/QuickSort.cpp
@@ -1,49 +1,71 @@
-#include <bits/stdc++.h>
+// C++ Implementation of the Quick Sort Algorithm.
+#include <iostream>
 using namespace std;
 
-void swap(int* a, int* b)
+int partition(int arr[], int start, int end)
 {
-	int t = *a;
-	*a = *b;
-	*b = t;
-}
-int partition(int arr[], int low, int high)
-{
-	int pivot = arr[high];
-	int i
-		= (low
-		- 1); 
 
-	for (int j = low; j <= high - 1; j++) {
-		if (arr[j] < pivot) {
-			i++; 
-			swap(&arr[i], &arr[j]);
+	int pivot = arr[start];
+
+	int count = 0;
+	for (int i = start + 1; i <= end; i++) {
+		if (arr[i] <= pivot)
+			count++;
+	}
+
+	// Giving pivot element its correct position
+	int pivotIndex = start + count;
+	swap(arr[pivotIndex], arr[start]);
+
+	// Sorting left and right parts of the pivot element
+	int i = start, j = end;
+
+	while (i < pivotIndex && j > pivotIndex) {
+
+		while (arr[i] <= pivot) {
+			i++;
+		}
+
+		while (arr[j] > pivot) {
+			j--;
+		}
+
+		if (i < pivotIndex && j > pivotIndex) {
+			swap(arr[i++], arr[j--]);
 		}
 	}
-	swap(&arr[i + 1], &arr[high]);
-	return (i + 1);
+
+	return pivotIndex;
 }
-void quickSort(int arr[], int low, int high)
+
+void quickSort(int arr[], int start, int end)
 {
-	if (low < high) {
-		int pi = partition(arr, low, high);
-		quickSort(arr, low, pi - 1);
-		quickSort(arr, pi + 1, high);
-	}
+
+	// base case
+	if (start >= end)
+		return;
+
+	// partitioning the array
+	int p = partition(arr, start, end);
+
+	// Sorting the left part
+	quickSort(arr, start, p - 1);
+
+	// Sorting the right part
+	quickSort(arr, p + 1, end);
 }
-void printArray(int arr[], int size)
-{
-	int i;
-	for (i = 0; i < size; i++)
-		cout << arr[i] << " ";
-	cout << endl;
-}
+
 int main()
 {
-	int arr[] = { 10, 7, 8, 9, 1, 5 };
-	int n = sizeof(arr) / sizeof(arr[0]);
+
+	int arr[] = { 9, 3, 4, 2, 1, 8 };
+	int n = 6;
+
 	quickSort(arr, 0, n - 1);
-	cout << "Sorted array: \n";
-	printArray(arr, n);
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+
 	return 0;
 }
